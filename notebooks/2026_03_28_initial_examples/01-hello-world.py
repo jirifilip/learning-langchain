b1f97b9b74659ffd45d3b16c03be6910a2cf77c5
Jiří Filip <j.f.ilip@seznam.cz>
2026-03-28 20:01:11 +0100

    Fix kernel name

--- notebooks/2026_03_28_initial_examples/01-hello-world.py
+++ notebooks/2026_03_28_initial_examples/01-hello-world.py
@@ -7,7 +7,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.19.1
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
